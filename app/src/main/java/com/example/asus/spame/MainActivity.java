42eec7202388d0cab502eb907adb559bdb3ea4a2
Johnedel <mapajohnedel@gmail.com>
2018-10-14 17:46:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/spame/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.asus.spame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    public Button buttonTest;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buttonTest = findViewById(R.id.buttonTest);
+        buttonTest.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonTest){
+            startActivity(new Intent(this, SignupActivity.class));
+        }
+    }
+}
